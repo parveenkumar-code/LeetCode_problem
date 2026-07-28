022a667bc4d1cec0652f95755b2a2b8aa6b6fd61
Parveen Kumar <parveen.kumar.b.tech.cse@gmail.com>
2026-07-28 17:50:08 +0530

    Add files via upload

---
+++ Easy Problems/Rotate String.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+
+        for i in range(len(s)):
+            s = s[1:] + s[0]
+
+            if s == goal:
+                return True
+
+        return False
